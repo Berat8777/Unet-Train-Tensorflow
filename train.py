09de3ef827ae43d959f4021254c4ac8cde9693a7
Berat8777 <150577533+Berat8777@users.noreply.github.com>
2024-02-23 19:51:05 +0300

    Create train.py

---
+++ train.py
@@ -0,0 +1,106 @@
+import os
+import numpy as np
+import cv2
+import tensorflow as tf
+from tensorflow.keras.layers import Input, Conv2D
+from tensorflow.keras.models import Model
+from tensorflow.keras.optimizers import Adam
+
+def image_load():
+    path = "/home/berat/Desktop/tusip_azaltılmıs/training/frames/"
+    image_folder = os.listdir(path)      #os ile dosyanın içindeki tüm dizinlerin bulunduğu bir liste yapar
+    img_liste = []
+    for i in image_folder[:100]:
+        img = cv2.imread(path+i)
+        img = cv2.resize(img,(512,512))
+        img = img / 255
+        img_liste.append(img)
+    return np.array(img_liste)     #verileri numpy dizinine çeviriyor 
+
+train_images = image_load()
+
+def mask_load():
+    path = "/home/berat/Desktop/tusip_azaltılmıs/training/lane-masks/"
+    mask_folder = os.listdir(path)
+    mask_liste = []
+    for i in mask_folder[:100]:
+        mask = cv2.imread(path+i,0)
+        mask = cv2.resize(mask,(512,512))
+        #mask = cv2.bitwise_not(mask)  (TERS MASKELER İÇİN )
+        mask = mask / 255
+        mask = np.expand_dims(mask,axis=-1)      #256,256nın yanına bir de 1 ekliyo
+        mask_liste.append(mask)
+    return np.array(mask_liste)
+
+mask_images = mask_load()
+
+def UNET (input_shape=(512,512,3),last_activation='sigmoid'):
+    
+
+    inputs=Input(shape=input_shape)
+
+    c1 = tf.keras.layers.Conv2D(64, (3, 3), activation='relu', kernel_initializer='he_normal', padding='same')(inputs)
+    c1 = tf.keras.layers.Dropout(0.1)(c1)
+    c1 = tf.keras.layers.Conv2D(64, (3, 3), activation='relu', kernel_initializer='he_normal', padding='same')(c1)
+    p1 = tf.keras.layers.MaxPooling2D((2, 2))(c1)
+
+    c2 = tf.keras.layers.Conv2D(128, (3, 3), activation='relu', kernel_initializer='he_normal', padding='same')(p1)
+    c2 = tf.keras.layers.Dropout(0.1)(c2)
+    c2 = tf.keras.layers.Conv2D(128, (3, 3), activation='relu', kernel_initializer='he_normal', padding='same')(c2)
+    p2 = tf.keras.layers.MaxPooling2D((2, 2))(c2)
+ 
+    c3 = tf.keras.layers.Conv2D(256, (3, 3), activation='relu', kernel_initializer='he_normal', padding='same')(p2)
+    c3 = tf.keras.layers.Dropout(0.2)(c3)
+    c3 = tf.keras.layers.Conv2D(256, (3, 3), activation='relu', kernel_initializer='he_normal', padding='same')(c3)
+    p3 = tf.keras.layers.MaxPooling2D((2, 2))(c3)
+    
+    c4 = tf.keras.layers.Conv2D(512, (3, 3), activation='relu', kernel_initializer='he_normal', padding='same')(p3)
+    c4 = tf.keras.layers.Dropout(0.2)(c4)
+    c4 = tf.keras.layers.Conv2D(512, (3, 3), activation='relu', kernel_initializer='he_normal', padding='same')(c4)
+    p4 = tf.keras.layers.MaxPooling2D(pool_size=(2, 2))(c4)
+    
+    c5 = tf.keras.layers.Conv2D(1024, (3, 3), activation='relu', kernel_initializer='he_normal', padding='same')(p4)
+    c5 = tf.keras.layers.Dropout(0.3)(c5)
+    c5 = tf.keras.layers.Conv2D(1024, (3, 3), activation='relu', kernel_initializer='he_normal', padding='same')(c5)
+
+    #Expansive path 
+    u6 = tf.keras.layers.Conv2DTranspose(512, (2, 2), strides=(2, 2), padding='same')(c5)
+    u6 = tf.keras.layers.concatenate([u6, c4])
+    c6 = tf.keras.layers.Conv2D(512, (3, 3), activation='relu', kernel_initializer='he_normal', padding='same')(u6)
+    c6 = tf.keras.layers.Dropout(0.2)(c6)
+    c6 = tf.keras.layers.Conv2D(512, (3, 3), activation='relu', kernel_initializer='he_normal', padding='same')(c6)
+    
+    u7 = tf.keras.layers.Conv2DTranspose(256, (2, 2), strides=(2, 2), padding='same')(c6)
+    u7 = tf.keras.layers.concatenate([u7, c3])
+    c7 = tf.keras.layers.Conv2D(256, (3, 3), activation='relu', kernel_initializer='he_normal', padding='same')(u7)
+    c7 = tf.keras.layers.Dropout(0.2)(c7)
+    c7 = tf.keras.layers.Conv2D(256, (3, 3), activation='relu', kernel_initializer='he_normal', padding='same')(c7)
+    
+    u8 = tf.keras.layers.Conv2DTranspose(128, (2, 2), strides=(2, 2), padding='same')(c7)
+    u8 = tf.keras.layers.concatenate([u8, c2])
+    c8 = tf.keras.layers.Conv2D(128, (3, 3), activation='relu', kernel_initializer='he_normal', padding='same')(u8)
+    c8 = tf.keras.layers.Dropout(0.1)(c8)
+    c8 = tf.keras.layers.Conv2D(128, (3, 3), activation='relu', kernel_initializer='he_normal', padding='same')(c8)
+    
+    u9 = tf.keras.layers.Conv2DTranspose(64, (2, 2), strides=(2, 2), padding='same')(c8)
+    u9 = tf.keras.layers.concatenate([u9, c1], axis=3)
+    c9 = tf.keras.layers.Conv2D(64, (3, 3), activation='relu', kernel_initializer='he_normal', padding='same')(u9)
+    c9 = tf.keras.layers.Dropout(0.1)(c9)
+    c9 = tf.keras.layers.Conv2D(64, (3, 3), activation='relu', kernel_initializer='he_normal', padding='same')(c9)
+
+    outputs = Conv2D(1,(1,1), activation = last_activation, padding = 'same', kernel_initializer = 'he_normal')(c9)
+
+    model = Model( inputs = inputs, outputs = outputs)
+
+    model.compile(optimizer=Adam(lr=1e-4), loss='binary_crossentropy', metrics=['accuracy'])
+    print(model.summary())
+
+    return model
+          
+model = UNET(input_shape=(512,512,3),last_activation="sigmoid")
+
+
+# Modeli eğit                    
+model.fit(train_images, mask_images, batch_size=4, epochs=50, validation_split=0.2)
+
+model.save("/home/berat1/Desktop/h5ler/deneme1.h5")
